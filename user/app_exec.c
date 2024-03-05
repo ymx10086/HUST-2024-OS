64308081f084dd83421dc4b5181bacd133281021
ymx10086 <1411477833@qq.com>
2024-03-05 13:53:37 +0800

    lab4_challenge2 is done

--- user/app_exec.c
+++ user/app_exec.c
@@ -1,26 +1,26 @@
-#include "user_lib.h"
-#include "util/types.h"
-
-int main(int argc, char *argv[]) {
-  printu("\n======== exec /bin/app_ls in app_exec ========\n");
-  int ret = exec("/bin/app_ls");
-  if (ret == -1)
-    printu("exec failed!\n");
-
-  exit(0);
-  return 0;
-}
-
 // #include "user_lib.h"
 // #include "util/types.h"
 
 // int main(int argc, char *argv[]) {
-//   printu("\n======== exec /bin/app_read in app_exec ========\n");
-//   printu("call exec in main\n");
-//   int ret = exec("/bin/app_read");
+//   printu("\n======== exec /bin/app_ls in app_exec ========\n");
+//   int ret = exec("/bin/app_ls");
 //   if (ret == -1)
 //     printu("exec failed!\n");
 
 //   exit(0);
 //   return 0;
 // }
+
+#include "user_lib.h"
+#include "util/types.h"
+
+int main(int argc, char *argv[]) {
+  printu("\n======== exec /bin/app_read in app_exec ========\n");
+  printu("call exec in main\n");
+  int ret = exec("/bin/app_read");
+  if (ret == -1)
+    printu("exec failed!\n");
+
+  exit(0);
+  return 0;
+}
